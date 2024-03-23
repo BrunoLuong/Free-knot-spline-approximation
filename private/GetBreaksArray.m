3e431e9ec37960de0ae61b52f93cc92c13ad78fc
BrunoLuong <57561200+BrunoLuong@users.noreply.github.com>
2024-03-23 15:24:46 +0100

    Update GetBreaksArray.m

--- private/GetBreaksArray.m
+++ private/GetBreaksArray.m
@@ -1,7 +1,7 @@
 function breaks = GetBreaksArray(i, nknots)
 % Construct the array of break indices of sorted x array
 % INPUTS:
-%   i: column vector of interval indices, must be ascending sorted, 
+%   i: column vector of interval indices, must be ascending sorted,
 %      unless it falls to 0
 %   nknots: scalar number of knots
 % OUTUT:
@@ -12,9 +12,10 @@ function breaks = GetBreaksArray(i, nknots)
 if isempty(i)
     breaks = zeros(1, nknots);
 else
-    jumpidx = find([true; diff(i)>0; true]);
     i(end+1) = nknots+1; % wanted this value for last element of ij
-                         % +1 so we be sure it will not repeat i(end)
+    % +1 so we be sure it will not repeat i(end)
+    jumpidx = find([true; diff(i)>0]);
+
     ij = i(jumpidx);
     breaks = interp1(ij, jumpidx, 1:nknots, 'next', 'extrap');
 end
