98c888945433fc234de11c214e8ae4142f0e73a6
BrunoLuong <57561200+BrunoLuong@users.noreply.github.com>
2024-03-23 15:20:32 +0100

    Update multMat.m (fix regression bug)

--- private/multMat.m
+++ private/multMat.m
@@ -1,9 +1,22 @@
 function Balpha = multMat(B, alpha)
-% %ake the matrix product B*alpha
-% Balpha = B*alpha; without returning NaN
-
-col = any(B,1);
-Balpha = B(:,col)*alpha(col,:);
+% Make the matrix "robust" product B*alpha
 % Balpha = B*alpha;
 
+if all(isfinite(alpha),'all')
+    Balpha = B*alpha;
+else
+    % Balpha = B*alpha;
+    % without returning NaN, since alpha ù*might contain Inf
+    col = any(B,1);
+    Balpha = B(:,col)*alpha(col,:);
+end
+
+if (issparse(B) || issparse(alpha)) % && ~issparse(Balpha)
+    % This seems to help later, , not sure why (!), but otherwise
+    % there might be a division by 0 in DerivBKnotDeriv()
+    % testBSFK(3) sometime reveals the flaw
+    Balpha = sparse(Balpha);
+end
+
 end % multMat
+
